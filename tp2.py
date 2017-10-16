4c1bf0c9b7a3141ffcf932e5b90fbd48be735792
El Kesri Othmane <elkesri.othmane@gmail.com>
2017-10-16 16:08:15 +0200

    Partie A TP2 done

--- tp2.py
+++ tp2.py
@@ -6,3 +6,15 @@ Created on Mon Oct  9 16:46:13 2017
 @author: 11710470
 """
 
+# Partie A
+
+from numpy import *
+from sklearn import preprocessing
+
+#1
+X = [[1, -1, 2], [2, 2, 0], [0, 1, -1]]
+#2
+X_scaled = preprocessing.scale(X)
+#3
+print np.mean(X_scaled)
+print np.var(X_scaled)
