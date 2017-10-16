7c291dbf6a33096b7ed7fdbd7818bed8a99aa2f2
El Kesri Othmane <elkesri.othmane@gmail.com>
2017-10-16 16:34:23 +0200

    TP2 Partie B done

--- tp2.py
+++ tp2.py
@@ -7,7 +7,7 @@ Created on Mon Oct  9 16:46:13 2017
 """
 
 # Partie A
-
+print "Partie A"
 from numpy import *
 from sklearn import preprocessing
 
@@ -18,3 +18,21 @@ X_scaled = preprocessing.scale(X)
 #3
 print np.mean(X_scaled)
 print np.var(X_scaled)
+
+
+#Partie B
+from sklearn.preprocessing import MinMaxScaler
+print "\nPartie B"
+#1
+X2 = [[1, -1, 2], [2, 0, 0], [0, 1, -1]]
+#2
+print "Moyenne sur les variables avant la normalisation"
+print np.mean(X2[0])
+print np.mean(X2[1])
+print np.mean(X2[2])
+#3
+print "Moyenne sur les variables après la normalisation"
+X2_scaled = MinMaxScaler([0,1]).fit_transform(X)
+print np.mean(X2_scaled[0])
+print np.mean(X2_scaled[1])
+print np.mean(X2_scaled[2])
