e5b515594e6e5702398144bd0cbf4ff78483e87b
EL KESRI Othmane <elkesri.othmane@gmail.com>
2017-11-27 15:15:55 +0100

    working on TP3

--- TP3.py
+++ TP3.py
@@ -8,32 +8,37 @@ Created on Mon Nov  6 16:19:43 2017
 
 # E(x) = x4 - 11x3 + 41x2 - 61x + 30
 
-from math import *
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import decimal 
 
 def E(x):
     return (x-1)*(x-2)*(x-3)*(x-5)
 
 def deriv_E(x):
-    return 4*math.pow(x, 3) - 33*math.pow(x,2) + 82*x -61
+    x = np.longdouble(x)
+    return 4*x**3 - 33*x**2 + 82*x -61
+
 
 # l'algorithme DG
-def DG(x0, eta, eps = 0.01, nb_max = 1000):    
-
-    xi = x0
-    yi = E(x0)
+def DG(x0, eta, eps = 0.01, nb_max_iter = 1000):    
+    plt.figure()
+    xi = np.longdouble(x0)
+    yi = np.longdouble(E(x0))
     plt.scatter(x0, E(x0))
+    ymin = yi
+    xmin = xi
     
-    cond = 50000
+    cond = eps + 20.0
     nb_iter = 0 
-    tmp_y = E(x0)
     while cond > eps and nb_iter < nb_max_iter:
-        cond = abs( -eta * deriv_E(xi) )
-        # to do ...
-        yi = xi - eta * deriv_E(xi)
-        # xi = xi - eta * deriv_E(xi)
+        xii = np.longdouble(xi - eta * deriv_E(xi) )
         yi = E(xi)
-        nb_iter = nb_iter + 1
-        tmp_y = y0
+        xmin = min([xmin, xii])
+        cond = abs( xii - xi )
+        xi = xii
+        nb_iter = nb_iter + 1        
         plt.scatter(xi, yi)
     
     plt.title("x0 = " + repr(x0) + " eta = " + repr(eta) )
@@ -41,10 +46,11 @@ def DG(x0, eta, eps = 0.01, nb_max = 1000):
     
     plt.savefig("gradient_descent_1d_python.png", bbox_inches='tight')
     plt.show()
+    print "Le minimum trouvé: ", xmin, " E(xmin) = ", E(xmin)
     
 DG(5, 0.001)
-DG(5, 0.01)
+DG(5, 0.01)                                                    
 DG(5, 0.1)
 DG(5, 0.17)
-DG(5, 1)
+DG(5, 1.0)
 DG(0, 0.001)
